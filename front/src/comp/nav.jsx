10a6cb279f7018dab14f7da36fc89d1fb737a7fc
SKYLAR BEESE <skylarbeese@gmail.com>
2021-04-19 22:46:10 -0600

    s

--- front/src/comp/nav.jsx
+++ front/src/comp/nav.jsx
@@ -1,10 +1,31 @@
 import {Link} from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
 import './Nav.css';
+
 function Nav() {
+  const [win, setWin] = useState(false)
+  const [btn, setBtn] = useState(false)
+  console.log(win)
+  console.log(btn)
+  const hand = () => {
+     if(window.innerWidth > 800) {
+       setWin(true)
+     } else {
+       setWin(false)
+     }
+   }
+  window.addEventListener("resize", hand);
+  useEffect(() => {
+     hand()
+ }, []);  
+ 
+ const handler = () => {
+  setBtn(!btn)
+}
     return (
      <>
     
-       <div className="nav-div">
+    {win ? (<div className="nav-div">
            <nav>
                <ul className="home-dir">
                <Link to="/"><li>Home</li></Link>
@@ -15,7 +36,22 @@ function Nav() {
                    <button>signup</button>
     </div> */}
            </nav>
-       </div>
+       </div>) : (<div className="nav-div-bur">
+         <div className={`nav-drop ${btn ? 'nav-show' : 'nav-hide'}`}>
+         
+              <ul className="col">
+               <Link to="/"><li>Home</li></Link>
+                 <Link to="/comp/addList"><li>Add listings</li></Link> 
+               </ul>
+        
+         </div>
+         <div className="burger" onClick={handler}>
+            <span className="line1"></span>
+            <span className="line1"></span>
+            <span className="line1"></span>
+            
+         </div>
+       </div>)}
      </>
     );
   }
